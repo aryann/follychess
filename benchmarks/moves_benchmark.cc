2d1730031c751fead020318fd620d1467e97abd9
Aryan Naraghi <aryan.naraghi@gmail.com>
2026-04-29 14:08:44 -0700

    Adds more positions to the moves benchmark

--- benchmarks/moves_benchmark.cc
+++ benchmarks/moves_benchmark.cc
@@ -59,9 +59,23 @@ BENCHMARK_CAPTURE(
     R"(r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1)")
     ->DenseRange(/* start = */ 1, /* limit = */ 5, /* step = */ 1);
 
+BENCHMARK_CAPTURE(BM_MakeMoves, Position3,
+                  R"(8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1)")
+    ->DenseRange(/* start = */ 1, /* limit = */ 5, /* step = */ 1);
+
+BENCHMARK_CAPTURE(
+    BM_MakeMoves, Position4,
+    R"(r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1)")
+    ->DenseRange(/* start = */ 1, /* limit = */ 5, /* step = */ 1);
+
 BENCHMARK_CAPTURE(
     BM_MakeMoves, Position5,
     R"(rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8)")
+    ->DenseRange(/* start = */ 1, /* limit = */ 5, /* step = */ 1);
+
+BENCHMARK_CAPTURE(
+    BM_MakeMoves, Position6,
+    R"(r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10)")
     ->DenseRange(/* start = */ 1, /* limit = */ 5, /* step = */ 1);
 
 }  // namespace
